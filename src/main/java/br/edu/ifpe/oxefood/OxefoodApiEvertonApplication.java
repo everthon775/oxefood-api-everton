3914ed41c2349b54e9d4886273d68bb2d7cc995c
Davi Pedro <dpsa1@discente.ifpe.edu.br>
2026-08-24 21:27:04 -0300

    primeiro commit

---
+++ src/main/java/br/edu/ifpe/oxefood/OxefoodApiEvertonApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiEvertonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiEvertonApplication.class, args);
+	}
+
+}
